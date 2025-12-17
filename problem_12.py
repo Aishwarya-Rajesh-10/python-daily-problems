8d81709a23ac2224f87366024f196198aca00f9b
Aishwarya-Rajesh-10 <aishwaryarajesh126@gmail.com>
2025-12-18 00:20:32 +0530

    add 12th program

---
+++ problem_12.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number:"))
+if a > 1:
+    for i in range(2, int(a ** 0.5) + 1):
+        if a % i == 0:
+            print(a, "is not a prime number")
+            break
+        else:
+            print("Its a prime number")
+
+
